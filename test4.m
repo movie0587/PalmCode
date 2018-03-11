535e80753b6c5356fbaf6fef9aecfcf5ec08a54b
WuCiel <871447603@qq.com>
2018-03-11 21:53:14 +0800

    1.modify PalmExtraction.m,make it more accurate. 2.add Gabor filter,but the argument not the best now

--- test4.m
+++ test4.m
@@ -1,10 +1,20 @@
-I0=imread('test.jpg');
-I = im2bw(I0,0.22);
-se = strel('square',3);
-Ie = imerode(I,se);
-Iout = I - Ie;
-figure;
-subplot(1,2,1);
-imshow(I);
-subplot(1,2,2);
-imshow(Iout);
+I = imread('D:\±ÏÉè\matlabCode\image\rtest_11.jpg');
+I=imresize(I,0.2);
+I=PalmExtraction(I);
+
+if ndims(I)>2
+    I=rgb2gray(I);
+end
+% figure,imshow(I);
+
+gaborArray = gabor([10 11 12 13 14],[90],'SpatialFrequencyBandwidth',1.5,'SpatialAspectRatio',6.0);
+
+gaborMag = imgaborfilt(I,gaborArray);
+
+for p = 1:5
+%     gaborMag(:,:,p)=imerode(gaborMag(:,:,p),strel('line',3,0));
+    figure,imshow(imbinarize(gaborMag(:,:,p)));
+    theta = gaborArray(p).Orientation;
+    lambda = gaborArray(p).Wavelength;
+    title(sprintf('Orientation=%d, Wavelength=%d',theta,lambda));
+end
--- test5.m
+++ test5.m
@@ -1,18 +1,19 @@
 clc; clear all;
 %% 参数设置
 thres_extraction_bw=0.29;
-thres_resize=0.5;
-imagePath='image/rtest_101.jpg';
+thres_resize=0.2;
+imagePath='image/rtest_121.jpg';
 
-%% 
+%% 手掌提取
 src=imread(imagePath);
 src=imresize(src,thres_resize);
 % src_gray=rgb2gray(src);
-figure('name','原图'),imshow(src),title('原图');
+% figure('name','原图'),imshow((src)),title('原图');
 
 palm=PalmExtraction(src,thres_extraction_bw);
 figure('name','提取出的手掌'),imshow(palm),title('提取出的手掌');
 
+%% Gabor滤波
 % palm_gray=rgb2gray(palm);
 % figure,imshow(palm_gray),title('palmgray');
 
